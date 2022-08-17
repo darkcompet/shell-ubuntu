1c9f8da4c3551198074b8709940baaa5949ff431
DarkCompet <darkcompet@gmail.com>
2022-08-17 22:37:29 +0700

    Update dotnet installation

--- installer.sh
+++ installer.sh
@@ -109,22 +109,7 @@ Install_Nginx() {
 Install_Dotnet() {
 	echo "[Info] Installing dotnet..."
 
-	# Install package
-	wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-	sudo dpkg -i packages-microsoft-prod.deb
-	rm packages-microsoft-prod.deb
-
-	# Install SDK for development
-	sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0
-
-	# Install runtime (asp.net core)
-	sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y aspnetcore-runtime-6.0
+	sudo apt-get update && sudo apt-get install -y dotnet6
 
 	echo "=> Installed dotnet."
 }
