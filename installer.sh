7ba24bc64bf841e2e79bcea5e5d3e50eaa989078
DarkCompet <darkcompet@gmail.com>
2022-11-30 11:20:25 +0700

    Add sudo for dotnet command

--- installer.sh
+++ installer.sh
@@ -166,9 +166,9 @@ Install_Dotnet6_ForUbuntu2204() {
 Install_Dotnet6_ForUbuntu2004() {
 	echo "[Info] Installing dotnet..."
 
-	wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+	sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 	sudo dpkg -i packages-microsoft-prod.deb
-	rm packages-microsoft-prod.deb
+	sudo rm packages-microsoft-prod.deb
 
 	sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
 
